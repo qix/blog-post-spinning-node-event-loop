3ed30eac105cd52bde7fab30e934cbb6edd05a50
Josh Yudaken <josh@smyte.com>
2018-04-11 21:53:57 -0700

    Update with new node modules

--- index.js
+++ index.js
@@ -1,12 +1,25 @@
-const EventLoopStats = require("./node-event-loop-stats").default;
-const { nextNice } = require("./node-nice");
+const { EventLoopMonitor } = require("node-event-loop-stats");
+const { nextNice, nicePromise } = require("node-nice");
+const docopt = require("docopt").docopt;
 
-const loopMonitor = new EventLoopStats("sample");
+function buildDoc(examples) {
+  const usage = examples
+    .map(name => `  ./example [options] ${name}`)
+    .join("\n");
+  return `
+Usage:
+${usage}
 
+Options:
+  --io=<io>        Time spent in IO [default: 0]
+  --work=<work>    Time spent doing work [default: 1]
+  --loops=<loops>  Number of loops to perform [default: 1]
+  --parallel=<n>   Number of examples to perform in parallel [default: 1]
+  --random-io      Randomly pick I/O time between 0*<io> and 2*<io> 
+`;
+}
 const NS_PER_MS = 1e6;
 const NS_PER_SEC = NS_PER_MS * 1e3;
-
-setTimeout(() => loopMonitor.start(), 1000);
 
 /***
  * This function emulates some kind of IO.
@@ -33,45 +46,100 @@ function computeForMillis(millis) {
   actualWorkNs += takenNs;
 }
 
-const strategies = {
-  normal: async (io, work) => {
-    while (true) {
+const examples = {
+  async simpleWorkLoop(io, work, workCount) {
+    for (let i = 0; i < workCount; i++) {
+      work();
+    }
+  },
+
+  async simpleIoWorkLoop(io, work, workCount) {
+    for (let i = 0; i < workCount; i++) {
       await io();
       work();
     }
   },
 
-  awaitNextNice: async (io, work) => {
-    while (true) {
+  async setImmediateLoop(io, work, workCount) {
+    for (let i = 0; i < workCount; i++) {
       await io();
       await new Promise(resolve => {
-        nextNice(() => {
+        setImmediate(() => {
           work();
-
           resolve();
         });
+      });
+    }
+  },
+
+  async niceIoWorkLoop(io, work, workCount) {
+    for (let i = 0; i < workCount; i++) {
+      await io();
+      await nicePromise(() => {
+        work();
       });
     }
   }
 };
 
-const strategy = strategies[process.argv[2]];
+const argv = docopt(buildDoc(Object.keys(examples)));
+const exampleName = Object.keys(examples).filter(name => argv[name]);
+const ioMs = parseFloat(argv["--io"]);
+const workMs = parseFloat(argv["--work"]);
+const loopCount = parseInt(argv["--loops"], 10);
+const parallelCount = parseInt(argv["--parallel"], 10);
 
-if (!strategy) {
-  console.error("Usage: ./time <" + Object.keys(strategies).join(" | ") + ">");
-  process.exit(1);
+const example = examples[exampleName];
+
+let io;
+if (argv["--random-io"]) {
+  io = () => emulateIoMillis(Math.random() * ioMs * 2);
+} else {
+  io = () => emulateIoMillis(ioMs);
 }
+const work = () => computeForMillis(workMs);
 
-const workStart = process.hrtime();
-const io = () => emulateIoMillis(Math.random() * 100);
-const work = () => computeForMillis(Math.random() * 0.5);
-for (let i = 0; i < 1000; i++) {
-  strategy(io, work);
-}
+(async function() {
+  const loopMonitor = new EventLoopMonitor({
+    timeoutMs: 0
+  });
 
-setInterval(() => {
-  const totalTime = process.hrtime(workStart);
-  const totalTimeNs = totalTime[0] * NS_PER_SEC + totalTime[1];
+  const logStats = () => {
+    const totalTime = process.hrtime(workStart);
+    const totalTimeNs = totalTime[0] * NS_PER_SEC + totalTime[1];
+    const usefulPercent = 100 * (actualWorkNs / totalTimeNs);
+    console.log(
+      `useful=${usefulPercent.toFixed(1)}% ` + `${loopMonitor.getStatsString()}`
+    );
+  };
 
-  console.log(actualWorkNs / totalTimeNs);
-}, 1000);
+  // Work is run in a hot loop, make sure whatever JIT needs to be done is done
+  // on it so that we get better timing
+  emulateIoMillis(1000);
+
+  // Startup can often introduce freezes. Give node some time to ensure that
+  // work is done and we get more accurate results.
+  await new Promise(setImmediate);
+  await new Promise(resolve => {
+    setTimeout(resolve, 1000);
+  });
+  await new Promise(setImmediate);
+
+  // Start statitistics/monitoring
+  const workStart = process.hrtime();
+  const statInterval = setInterval(logStats, 1000);
+  loopMonitor.start();
+  actualWorkNs = 0;
+
+  const promises = new Array(parallelCount);
+  for (let i = 0; i < parallelCount; i++) {
+    promises[i] = example(io, work, loopCount);
+  }
+
+  await Promise.all(promises);
+  await new Promise(setImmediate);
+
+  clearInterval(statInterval);
+  logStats();
+  loopMonitor.stop();
+})();
